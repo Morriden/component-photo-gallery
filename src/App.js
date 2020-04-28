a225ed2d2d7363fa8e6d47ec2d7cca476094f0a4
Erik Ford <ekford18@gmail.com>
2020-04-28 15:07:22 -0700

    completed app.js page, seems correct and shows form choices. Working on horn currently.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import hornData from './data.js';
+import Horn from './horn.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component { 
+  
+  state = { selected: null };
+
+  hornChange = (e) => {
+    this.setState({ selected: e.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <header>
+          <img src="" alt=""/>
+          <h1>The Horn to Rule All Horns</h1>
+        </header>
+
+        <main>
+          <section className="hornOptions">
+            <select className="cat-type-filter" onChange={this.handleChange}>
+              <option value="" defaultValue>
+                All Types
+              </option>
+              <option value="narwhal">Narwhal Horns</option>
+              <option value="rhino">Rhino Horns</option>
+              <option value="unicorn">Unicorn Horns</option>
+              <option value="unilego">Unilego Horns</option>
+              <option value="triceratops">Triceratops Horns</option>
+              <option value="markhor">Markhor Horns</option>
+              <option value="mouflon">Mouflon Horns</option>
+              <option value="addax">Addax Horns</option>
+              <option value="chameleon">Chameleon Horns</option>
+              <option value="lizard">Lizard Horns</option>
+              <option value="dragon">Dragon Horns</option>
+            </select>
+          </section>
+          <section className="list-section">
+            <ul className="horns">
+              {
+                hornData
+                  .filter(keyword => {
+                    if (!this.state.selected) return true;
+                    return keyword.keyword === this.state.selected;
+                  })
+                  .map(image => {
+                    return <Horn image={image} />
+                  })
+              }
+
+            </ul>
+
+          </section>
+
+        </main>
+
+      </div>            
+
+
+
+
+
+    );
+  }
 }
 
-export default App;
